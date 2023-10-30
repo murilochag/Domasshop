056985c35f311741ecfb9891a51cbbf8bd06f725
murilochag <murilochagas10@outlook.com>
2023-10-30 11:30:16 -0300

    criação do formulário e botão abir formulário

--- app/src/main/java/com/murilochag/domasshop/MainActivity.kt
+++ app/src/main/java/com/murilochag/domasshop/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.murilochag.domasshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.murilochag.domasshop.adapter.CustomAdapter
 import com.murilochag.domasshop.model.Produto
 
@@ -20,6 +23,14 @@ class MainActivity : AppCompatActivity() {
 
         lista_produto_activity.adapter = CustomAdapter(this,produtos)
         lista_produto_activity.layoutManager = LinearLayoutManager(this)
+
+
+        val floatingActionButtonAdd = findViewById<FloatingActionButton>(R.id.floatingActionButtonAdd)
+
+        floatingActionButtonAdd.setOnClickListener{
+            val formularioActivity = Intent(this, FormularioActivity::class.java)
+            startActivity(formularioActivity)
+        }
 
     }
 }
